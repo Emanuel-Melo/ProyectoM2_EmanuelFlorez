47098ae881d41afd273b5994eeea663ab899674c
Emanuel <emanuel.florez1016@gmail.com>
2026-04-20 13:11:56 -0500

    Se hicieron cambios en index.js prioritariamente por middleware inline, ademas de dejar el codigo mas organizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authorsRoutes from "./routes/authors-routes.js";
 import postsRoutes from "./routes/posts-routes.js";
+import errorHandler from "./middlewares/error-handler.js";
+import pool from "./db/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,23 +13,15 @@ app.get("/", (req, res) => {
     res.send("API funcionando 🚀");
 });
 
-// Rutas
 app.use("/authors", authorsRoutes);
 app.use("/posts", postsRoutes);
 
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error" });
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
-
-
-import pool from "./db/db.js";
+app.use(errorHandler);
 
 pool.connect()
     .then(() => console.log("✅ Conectado a PostgreSQL"))
     .catch(err => console.error("❌ Error de conexión", err));
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
