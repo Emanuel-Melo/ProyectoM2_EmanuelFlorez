372426dea0476f947f20aaf85594701e2ca031d7
Emanuel <emanuel.florez1016@gmail.com>
2026-04-20 11:03:06 -0500

    Se cambiaron algunas partes del código de index.js por cuestiones de uso de requiere a import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import authorsRoutes from "./routes/authors.routes.js";
+import postsRoutes from "./routes/posts.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,12 +11,9 @@ app.get("/", (req, res) => {
     res.send("API funcionando 🚀");
 });
 
+app.use("/authors", authorsRoutes);
+app.use("/posts", postsRoutes);
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-const authorsRoutes = require('./routes/authors.routes');
-const postsRoutes = require('./routes/posts.routes');
-
-app.use('/authors', authorsRoutes);
-app.use('/posts', postsRoutes);
