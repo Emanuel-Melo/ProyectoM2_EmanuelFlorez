84778a4b6db9fd9ab01e4fd38c4e35d7302b7b0a
Emanuel <emanuel.florez1016@gmail.com>
2026-04-20 11:13:46 -0500

    Se añadio un endpoint en authors y en posts, se cambio de requiere a import

--- src/routes/posts-routes.js
+++ src/routes/posts-routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from "express";
 const router = express.Router();
 
 let posts = [
@@ -61,3 +61,4 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+export default router;
