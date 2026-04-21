43d8858b90aecd418a032640c226e80f3de15eac
Emanuel <emanuel.florez1016@gmail.com>
2026-04-21 10:15:32 -0500

    Se edito comments-service para postgresql

--- src/services/comments-service.js
+++ src/services/comments-service.js
@@ -7,20 +7,37 @@ export const getAllComments = async () => {
     return result.rows;
 };
 
+export const getCommentById = async (id) => {
+    const result = await pool.query(
+        "SELECT * FROM comments WHERE id = $1",
+        [id]
+    );
+    return result.rows[0];
+};
+
 export const getCommentsByPost = async (postId) => {
     const result = await pool.query(
-        "SELECT * FROM comments WHERE post_id = $1",
+        "SELECT * FROM comments WHERE post_id = $1 ORDER BY id",
         [postId]
     );
     return result.rows;
 };
 
-export const createComment = async (post_id, author_name, content) => {
+export const createComment = async (content, author_id, post_id) => {
     const result = await pool.query(
-        `INSERT INTO comments (post_id, author_name, content)
+        `INSERT INTO comments (content, author_id, post_id)
         VALUES ($1, $2, $3)
          RETURNING *`,
-        [post_id, author_name, content]
+        [content, author_id, post_id]
+    );
+
+    return result.rows[0];
+};
+
+export const deleteComment = async (id) => {
+    const result = await pool.query(
+        "DELETE FROM comments WHERE id = $1 RETURNING *",
+        [id]
     );
 
     return result.rows[0];
