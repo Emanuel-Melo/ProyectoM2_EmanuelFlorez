f5fd23699c899721c3bfd6452e2c0f4d5fd22ebb
Emanuel <emanuel.florez1016@gmail.com>
2026-04-20 21:00:10 -0500

    Se hicieron cambio es index en funcion de unos warning que aparecieron mientras se ponia a funcionar el supertest

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import express from "express";
 import authorsRoutes from "./routes/authors-routes.js";
 import postsRoutes from "./routes/posts-routes.js";
 import errorHandler from "./middlewares/error-handler.js";
-import pool from "./db/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -17,13 +15,5 @@ app.use("/authors", authorsRoutes);
 app.use("/posts", postsRoutes);
 
 app.use(errorHandler);
-
-pool.connect()
-    .then(() => console.log("✅ Conectado a PostgreSQL"))
-    .catch(err => console.error("❌ Error de conexión", err));
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
 
 export default app;
