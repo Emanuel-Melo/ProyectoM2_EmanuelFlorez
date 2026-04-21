1fa9d8c6eb59d80397d75e628a64c60fa9c30337
Emanuel <emanuel.florez1016@gmail.com>
2026-04-21 10:26:50 -0500

    Se edito index por fallas al iniciar server, reestructuración y mejor organización

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import express from "express";
 import authorsRoutes from "./routes/authors-routes.js";
 import postsRoutes from "./routes/posts-routes.js";
-import errorHandler from "./middlewares/error-handler.js";
 import commentsRoutes from "./routes/comments-routes.js";
-
-app.use("/comments", commentsRoutes);
+import errorHandler from "./middlewares/error-handler.js";
+import pool from "./db/db.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use("/authors", authorsRoutes);
+app.use("/posts", postsRoutes);
+app.use("/comments", commentsRoutes);
 
 app.get("/", (req, res) => {
     res.send("API funcionando 🚀");
 });
 
-app.use("/authors", authorsRoutes);
-app.use("/posts", postsRoutes);
-
 app.use(errorHandler);
+
+pool.connect()
+    .then(() => console.log("✅ Conectado a PostgreSQL"))
+    .catch(err => console.error("❌ Error de conexión", err));
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
 export default app;
