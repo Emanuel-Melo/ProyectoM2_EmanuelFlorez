34ab4e4981721b0c1ad9f24b3867949ef91eab74
Emanuel <emanuel.florez1016@gmail.com>
2026-04-21 10:44:47 -0500

    Correción a server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import app from "./index.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
+
+server.on("error", (err) => {
+    console.error("❌ Error al iniciar el servidor:", err.message);
 });
