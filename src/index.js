3bccdd47ca12174a016658fff6b6447910a4a5c3
Emanuel <emanuel.florez1016@gmail.com>
2026-04-21 10:46:46 -0500

    Correción a index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import authorsRoutes from "./routes/authors-routes.js";
 import postsRoutes from "./routes/posts-routes.js";
 import commentsRoutes from "./routes/comments-routes.js";
 import errorHandler from "./middlewares/error-handler.js";
-import pool from "./db/db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -15,17 +13,9 @@ app.use("/posts", postsRoutes);
 app.use("/comments", commentsRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API funcionando 🚀");
+    res.json({ message: "API funcionando 🚀" });
 });
 
 app.use(errorHandler);
-
-pool.connect()
-    .then(() => console.log("✅ Conectado a PostgreSQL"))
-    .catch(err => console.error("❌ Error de conexión", err));
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
 
 export default app;
