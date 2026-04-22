c4b6a249214280eafb666c558abf464e7bf532c7
Emanuel <emanuel.florez1016@gmail.com>
2026-04-22 10:10:33 -0500

    Cambio de codigo por fallo servidor

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
-import pkg from "pg";
-const { Pool } = pg;
-
+import { Pool } from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
