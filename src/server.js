a3a05ce30d66d77ff4707027015f8f6c83f753fc
Emanuel <emanuel.florez1016@gmail.com>
2026-04-21 19:15:26 -0500

    Se cambio unas cosas de server.js en funcion de raylway

--- src/server.js
+++ src/server.js
@@ -1,17 +1,7 @@
 import app from "./index.js";
-import pool from "./db/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-// Conectar DB primero
-pool.connect()
-    .then(() => {
-        console.log("✅ Conectado a PostgreSQL");
-
-        app.listen(PORT, () => {
-        console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error("❌ Error de conexión a PostgreSQL:", err.message);
-    });
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
