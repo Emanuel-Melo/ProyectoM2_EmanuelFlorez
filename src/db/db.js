40a90fe9b83ba78f3f10583af68d40779574bd25
Emanuel <emanuel.florez1016@gmail.com>
2026-04-21 21:03:16 -0500

    Se hizo cambios en db y en server por funcion de servidor en railwayn

--- src/db/db.js
+++ src/db/db.js
@@ -1,23 +1,9 @@
 import pkg from "pg";
-
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
-
-pool.connect()
-    .then(client => {
-        console.log("✅ DB conectada");
-        client.release();
-    })
-    .catch(err => {
-        console.error("❌ Error DB:", err);
-    });
 
 export default pool;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import app from "./index.js";
+import pool from "./db/db.js";
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+pool.query("SELECT NOW()")
+    .then(() => console.log("✅ DB conectada"))
+    .catch(err => console.error("❌ DB error:", err));
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
